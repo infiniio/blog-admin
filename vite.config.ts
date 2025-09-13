7578ae883b9458f7fefff14ee4863221a37f9ccb
aflahsidhique <81468620+aflahsidhique@users.noreply.github.com>
2025-09-13 08:04:52 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://blog-admin-i1kr.onrender.com',
         changeOrigin: true,
         secure: false,
       },
